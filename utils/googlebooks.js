8ac8ac0335668719868f53f27faa0c15b4ba16f9
James Knight <james.p.knight2@gmail.com>
2019-02-12 09:31:27 -0700

    Add files via upload

---
+++ utils/googlebooks.js
@@ -0,0 +1,20 @@
+const axios = require('axios')
+
+module.exports = async (phrase) => {
+    const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+        params: {
+            format: 'json',
+            q: `${phrase}`
+        }
+    })
+    
+    let myObj = { 
+	 data: results.data,
+	 status: results.status,
+	 statusText: results.statusText,
+	 headers: results.headers,
+	 requestHeader: results.config.headers
+    }
+
+    return JSON.stringify(myObj.data); 
+}
